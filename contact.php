fbe2622072de1ccdf7ab2562666aa31e24154d75
KsJjjjj <kjszywala@protonmail.com>
2023-01-12 21:46:35 +0000

    Email done

--- contact.php
+++ contact.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    include 'phpscripts.php';
+?>
 <html>
 <head>
   <meta charset="utf-8">
@@ -21,25 +25,37 @@
                 </tr>
                 <tr>
                     <td><b>Full Name:</b></td>
-                    <td><input type="text" name="fullname" maxlength="255" id="textbox"/></td>
+                    <td><input type="text" name="fullname" maxlength="255" id="textbox" required/></td>
                 </tr>
                 <tr>
                     <td><b>E-mail:</b></td>
-                    <td><input type="text" name="email" maxlength="255" id="textbox"/></td>
+                    <td><input type="text" name="email" maxlength="255" id="textbox" required/></td>
                 </tr>
                 <tr>
                     <td><b>Message:</b></td><br>
                 </tr>
                 <tr>
-                    <td colspan="2" style="width: 100%;"><textarea name="message" maxlength="255" id="messagetextbox"></textarea></td>
+                    <td colspan="2" style="width: 100%;"><textarea name="message" maxlength="255" id="messagetextbox" required></textarea></td>
                 </tr>
                 <tr>
                     <td colspan="2" style="padding-top: 30px;">
-                    <input type=submit name="singUp" id="test" value='SUBMIT' style='width: 100%;'></td>
+                    <input type=submit name="submit" id="test" value='SUBMIT' style='width: 100%;'></td>
                 </tr>
             </table> 
         </form>
     </center>
+    <?php
+        if(isset($_POST['submit'])){
+            $to = "kjszywala@protonmail.com";
+            $header = "MIME-Version: 1.0" . "\r\n";
+            $header .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            $header .= 'From: <'. $_POST['email'] . ">" . "\r\n";
+            $header .= 'Name: <'. $_POST['fullname'] . ">" . "\r\n";
+            $subject = "Email";
+            $message = $_POST['message'];
+            mail($to,$subject,$message,$header);
+        }
+    ?>
 </div>
 </body>
 </html>
